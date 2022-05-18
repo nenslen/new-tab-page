60b60608eb4269821731fc210a249027035f0ab1
Nic Enslen <nicolas.enslen@gmail.com>
2022-05-18 10:31:35 -0700

    Added search bars, changed background, removed trello

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	$('.search-input').keypress(function(event){
+		let keycode = (event.keyCode ? event.keyCode : event.which);
+		
+		if (keycode == '13') {
+			let url = $(this).data('url');
+			let query = $(this).val();
+			let search_url = `https://www.google.com/search?q=site:${url}+${query.replaceAll(' ', '+')}`;
+			window.location.href = search_url;
+		}
+	});
+
+
 	function showWeather() {			
 		let apiKey = 'd52e48ecf945475ecb51e3242de2bee3';
 		let url = 'https://api.darksky.net/forecast/';
@@ -49,42 +61,6 @@ $(document).ready(function(){
 		});
 	}
 
-	function showTrello() {
-		let taskListId = '5e9ff5ee4db7d721a0316d5c';
-		let workListId = '5d3213b8a43ebd8b88981a75';
-		let url = 'https://api.trello.com/1/lists/' + taskListId + '/cards?key=' + trelloKey + '&token=' + trelloToken;
-		
-		$.getJSON(url, function(cards) {
-			for (let i = 0; i < cards.length; i++) {
-				let card = '<a href="https://trello.com/b/XHVVmMKx/tasks"><div class="trello-card">';
-				card += cards[i].name;
-				card += '</div></a>';
-
-				$('#trello').append(card);
-			}
-		});
-
-		let d = new Date();
-		let day = d.getDay();
-		let hour = d.getHours();
-
-		// Show work tasks if current time is during work hours
-		if (day != 0 && day != 6 && hour >= 10 && hour <= 16) {
-			let workListId = '5d3213b8a43ebd8b88981a75';
-			let url = 'https://api.trello.com/1/lists/' + workListId + '/cards?key=' + trelloKey + '&token=' + trelloToken;
-
-			$.getJSON(url, function(cards) {
-				for (let i = 0; i < cards.length; i++) {
-					let card = '<a href="https://trello.com/b/4kymiHJV/ddc"><div class="trello-card trello-card-work">';
-					card += cards[i].name;
-					card += '</div></a>';
-
-					$('#trello').append(card);
-				}
-			});
-		}
-	}
-
 	function getDayFromTimestamp(timestamp) {
 		let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 		let day = (new Date(timestamp * 1000)).getDay()
@@ -92,5 +68,4 @@ $(document).ready(function(){
 	}
 
 	showWeather();
-	showTrello();
 });
